3466b10ba5247df8a90820ac07c51d99f3f77fb6
Matthias Neumayer <neumayer.matt@gmail.com>
2018-11-20 12:17:10 +0100

    WASAPI working

--- test.py
+++ test.py
@@ -7,35 +7,44 @@ if len(sys.argv) < 2:
     print("Plays a wave file.\n\nUsage: %s filename.wav" % sys.argv[0])
     sys.exit(-1)
 
-wf = wave.open(sys.argv[1], 'rb')
-wfout = wave.open("out.wav", 'wb')
-wfout.setnchannels(2)
-wfout.setsampwidth(2)
-wfout.setframerate(44100)
-
 # instantiate PyAudio (1)
+print(pyaudio.paWASAPI)
 p = pyaudio.PyAudio()
+
+wasapi_info = p.get_host_api_info_by_type(pyaudio.paWASAPI)
+print(wasapi_info)
+
+for i in range(p.get_device_count()):
+    print(p.get_device_info_by_index(i))
+input_device = p.get_device_info_by_index(wasapi_info['defaultInputDevice'])
+output_device = p.get_device_info_by_index(wasapi_info['defaultOutputDevice'])
+
+print(input_device)
+print(output_device)
 
 # define callback (2)
 def callback(in_data, frame_count, time_info, status):
-    #data = wf.readframes(frame_count)
-    #print(time_info)
-    wfout.writeframes(in_data)
-    #print(len(in_data))
+    print(time_info)
     return (in_data, pyaudio.paContinue)
 
 
+print(p.is_format_supported(int(output_device['defaultSampleRate']),
+                            input_device=wasapi_info['defaultInputDevice'],
+                            input_channels=3,
+                            input_format=pyaudio.paInt16,
+                            output_device=wasapi_info['defaultOutputDevice'],
+                            output_channels=2,
+                            output_format=pyaudio.paInt16))
 
 # open stream using callback (3)
-stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                channels=wf.getnchannels(),
-                rate=44100,
+stream = p.open(format=pyaudio.paInt16,
+                channels=2,
+                rate=int(output_device['defaultSampleRate']),
                 output=True,
                 input=True,
+                input_device_index=wasapi_info['defaultInputDevice'],
+                output_device_index= wasapi_info['defaultOutputDevice'],
                 stream_callback=callback)
-#print(p.get_default_input_device_info())
-#print(p.get_sample_size(p.get_default_input_device_info()))
-
 
 # start the stream (4)
 stream.start_stream()
@@ -47,8 +56,6 @@ while stream.is_active():
 # stop stream (6)
 stream.stop_stream()
 stream.close()
-wf.close()
-wfout.close()
 
 # close PyAudio (7)
 p.terminate()
